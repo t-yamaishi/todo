0c23b022cd8fbb5ec48d552ef7b3f4de61375265
yamaishi <yamakios8@icloud.com>
2022-11-30 09:25:24 +0900

    scaffoldでできた不要なファイルを削除

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'webpacker', '~> 5.0'
 
 gem 'turbolinks', '~> 5'
 
-gem 'jbuilder', '~> 2.7'
+#gem 'jbuilder', '~> 2.7'
 
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,42 +20,32 @@ class PostsController < ApplicationController
   def edit
   end
 
-  # POST /posts or /posts.json
+  # POST /posts
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+
+    if @post.save
+      redirect_to @post, notice: "Post was successfully created."
+    else
+      render :new
     end
+
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
+  # PATCH/PUT /posts/1
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render :edit
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
+  # DELETE /posts/1
   def destroy
     @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to posts_url, notice: "Post was successfully destroyed."
   end
 
   private
