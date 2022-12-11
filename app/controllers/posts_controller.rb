1f91a52aac2987ffed77d5d92d82177ab7b5548c
yamaishi <yamakios8@icloud.com>
2022-12-11 12:42:21 +0900

    タグ機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,6 +62,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:content, :deadline, {tag_id: []})
+      params.require(:post).permit(:content, :deadline, :tag_ids  )
     end
 end
